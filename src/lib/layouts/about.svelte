721c8d1dd2e9f2e9fe9c914939fd9b12e152c2ea
dahnte <dantencardenas@gmail.com>
2026-02-27 22:38:00 -0800

    favicon and about change

--- src/lib/layouts/about.svelte
+++ src/lib/layouts/about.svelte
@@ -2,6 +2,6 @@
     let { title, author, children } = $props();
 </script>
 
-<section class="font-[Libertinus_Sans] prose text-xl m-8 sm:m-15 max-w-full items-center gap-10 object-contain">
+<section class="font-[Libertinus_Sans] prose text-xl m-8 sm:m-18 max-w-full items-center gap-10 object-contain">
     {@render children()}
 </section>
